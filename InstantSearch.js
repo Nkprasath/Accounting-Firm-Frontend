96cf822c41d5f8c803c77e08d2b95dc18a955e87
Nakendraprasath K <nakendraprasath321@gmail.com>
2020-11-06 08:44:56 -0800

    Update InstantSearch.js

--- InstantSearch.js
+++ InstantSearch.js
@@ -25,23 +25,94 @@ class InstantSearch{
   }
 
   addListerners() {
-    let delay:
+    let delay;
 
-    this.elements.input.addEventListener("input", () => {
+      this.elements.input.addEventListener("input", () => {
       clearTimeout(delay):
 
       const query = this.elements.input.value;
 
       delay = setTimeout(() =>{
-      consle.log(query);
+      if(query.lenght < 3) {
+        this.populateResults([]);
+        return;
+      }
+
+      this.performSearch(query).then(results => {
+        this.populateResults(results);
+      });
       },500);
 
     });
+
+    this.elements.input.addEventListener("focus", () => {
+      this.elements.resultContainer.classList.add("instant-search__results-containter--visible");
+    });
+    this.elements.input.addEventListener("blur", () => {
+      this.elements.resultContainer.classList.remove("instant-search__results-containter--visible");
+    });
+
 }
 
-  populateResults(){
+/**
+ * @param {Object[]} results
+ */
 
+  populateResults(){
+    while (this.elements.resultContainer.firstChild){
+      this.elements.resultContainer.removeChild(this.element.resultContainer.firstChild);
+    }
+
+    for (const result of results){
+      this.element.resultContainer.appendChild(this.createResultElement(result));
+    }
+
+}
+
+createResultElement(result) {
+  const anchorElement = document.createElement("a");
+  anchorElement.classList.add("instant-search__result");
+  anchorElement.insertAdjacentHTML("afterbegin", this.options.templateFunction(result));
+
+  if("href" in result){
+    anchorElement.setAttribute("href", result.href);
   }
+  return anchorElement;
+}
+
+
+    /**
+     * @param {string} query Search query
+     *@returns {Promise<Object[]>}
+     */
+    performSearch(query){
+      const url = new URL(this.options.searchUrl.toString());
+      url.searchParams.set(this.option.queryParam, query);
+      this.setLoading(true);
+      return fetch(url, {
+        method:"get"
+      }).then(response => {
+        if(response.status !== 200){
+          throw new Error("Something went wrong with the search!");
+        }
+        return response.json();
+      }).then(responseData => {
+        console.log(responseData);
+        return this.options.responseParser(responseData);
+      }).catch(error => {
+        console.error(error);
+        return [];
+      }).finally(results => {
+        this.setLoading(false);
+        return results;
+      });
+    }
+    /**
+     * @param {boolean} b
+     */
+     setLoading(b){
+       this.elements.main.classList.toggle("instant-search--loading",b);
+     }
 
 
 export default InstantSearch;
