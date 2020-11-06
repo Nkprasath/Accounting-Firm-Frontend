c7d1a40b51279c2804b75eaaeeb98c9fdb32256e
Nakendraprasath K <nakendraprasath321@gmail.com>
2020-11-06 07:03:51 -0800

    Delete instant-search.js

--- instant-search.js
+++
@@ -1,9 +0,0 @@
-import InstantSearch from "./InstantSearch.js"
-
-const searchUser = document.querySelector("#searchServices");
-const InstantSearchServices = new InstantSearch(searchUsers,{
-  searchUrl: new URL("/search.php", window.location.origin),
-  queryParam = 'q',
-  resultParser: () => {},
-  templateFunction: () => {}
-} );
