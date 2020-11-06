c17a21969b8dabf05460cda6114784af4c0b437d
Nakendraprasath K <nakendraprasath321@gmail.com>
2020-11-06 07:00:04 -0800

    Update instant-search.js

--- instant-search.js
+++ instant-search.js
@@ -1,6 +1,6 @@
 Import InstantSearch from "./InstantSearch.js"
 
-const searchUser = document.querySelector("#searchServices")
+const searchUser = document.querySelector("#searchServices");
 const InstantSearchServices = new InstantSearch(searchUsers,{
   searchUrl: new URL("/search.php", window.location.origin),
   queryParam = 'q',
