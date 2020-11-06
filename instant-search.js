1343b4d586ac8fc531dbef8b49950152311ae4f1
Nakendraprasath K <nakendraprasath321@gmail.com>
2020-11-06 06:57:10 -0800

    Create instant-search.js

---
+++ instant-search.js
@@ -0,0 +1,9 @@
+Import InstantSearch from "./InstantSearch.js"
+
+const searchUser = document.querySelector("#searchServices")
+const InstantSearchServices = new InstantSearch(searchUsers,{
+  searchUrl: new URL("/search.php", window.location.origin),
+  queryParam = 'q',
+  resultParser: () => {},
+  templateFunction: () => {}
+} );
