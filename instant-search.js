fe711b07d90622af52f4f1740e8f62ee2535e8df
Nakendraprasath K <nakendraprasath321@gmail.com>
2020-11-06 07:49:29 -0800

    Update instant-search.js

--- instant-search.js
+++ instant-search.js
@@ -3,7 +3,7 @@ import InstantSearch from "./InstantSearch.js"
 const searchUser = document.querySelector("#searchServices");
 const InstantSearchServices = new InstantSearch(searchUsers,{
   searchUrl: new URL("/search.php", window.location.origin),
-  queryParam = "q",
+  queryParam : "q",
   resultParser: () => {},
   templateFunction: () => {}
 } );
